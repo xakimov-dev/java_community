41522261c353a4cb85f2b7b7153a48440b5e8fa3
shoxrux_dev <niyozovshoxrux277@gmail.com>
2023-05-01 09:30:34 +0300

    test

--- src/test/java/uz/community/javacommunity/controller/article/ArticleUpdateTest.java
+++ src/test/java/uz/community/javacommunity/controller/article/ArticleUpdateTest.java
@@ -7,9 +7,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uz.community.javacommunity.controller.domain.Article;
+import uz.community.javacommunity.controller.domain.Category;
 import uz.community.javacommunity.controller.dto.ArticleResponse;
 import uz.community.javacommunity.controller.dto.ArticleUpdateRequest;
 import uz.community.javacommunity.controller.repository.ArticleRepository;
+import uz.community.javacommunity.controller.repository.CategoryRepository;
 import uz.community.javacommunity.service.ArticleService;
 
 
@@ -19,19 +21,33 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class ArticleUpdateTest {
 
     @Mock
     private ArticleRepository articleRepository;
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
     @InjectMocks
     private ArticleService articleService;
 
     private Article article;
+    private Category category;
 
     @BeforeEach
     public void setUp() {
+        category = new Category(
+                UUID.randomUUID(),
+                "Category 1",
+                null,
+                "a",
+                Instant.now(),
+                "",
+                Instant.now()
+                );
         article = new Article(
                 new Article.ArticleKey(),
                 "Test Article",
@@ -48,7 +64,7 @@ class ArticleUpdateTest {
         String username = "a";
         ArticleUpdateRequest updateRequest = new ArticleUpdateRequest(new Article.ArticleKey(), username);
 
-
+        when(categoryRepository.save(category)).thenReturn(category);
         when(articleRepository.findById(article.getArticleKey())).thenReturn(Optional.of(article));
         when(articleRepository.save(article)).thenReturn(article);
 
