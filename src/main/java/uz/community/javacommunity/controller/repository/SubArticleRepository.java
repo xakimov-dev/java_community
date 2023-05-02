a7fd5de5d209ed23cea7c398b5ec4315777cc222
xakimov <xakimov.dev@gmail.com>
2023-05-02 22:12:08 +0500

    some changes

--- src/main/java/uz/community/javacommunity/controller/repository/SubArticleRepository.java
+++ src/main/java/uz/community/javacommunity/controller/repository/SubArticleRepository.java
@@ -3,6 +3,10 @@ package uz.community.javacommunity.controller.repository;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import uz.community.javacommunity.controller.domain.SubArticle;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface SubArticleRepository extends CassandraRepository<SubArticle, SubArticle.SubArticleKey> {
     boolean existsByName(String name);
+    Optional<SubArticle> findBySubArticleKeyId(UUID id);
 }
--- src/main/java/uz/community/javacommunity/service/SubArticleService.java
+++ src/main/java/uz/community/javacommunity/service/SubArticleService.java
@@ -3,10 +3,13 @@ package uz.community.javacommunity.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uz.community.javacommunity.common.exception.AlreadyExistsException;
+import uz.community.javacommunity.common.exception.RecordNotFoundException;
 import uz.community.javacommunity.controller.domain.SubArticle;
 import uz.community.javacommunity.controller.dto.SubArticleRequest;
 import uz.community.javacommunity.controller.dto.SubArticleResponse;
 import uz.community.javacommunity.controller.repository.SubArticleRepository;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,13 @@ public class SubArticleService {
         }
         SubArticle savedSubArticle = repository.save(SubArticle.of(dto));
         return SubArticleResponse.of(savedSubArticle);
+    }
+
+    public SubArticle delete(UUID subArticleId){
+        SubArticle subArticle = repository.findBySubArticleKeyId(subArticleId).orElseThrow(() -> {
+            throw new RecordNotFoundException("SubArticle with id '" + subArticleId + "' cannot be found");});
+        repository.delete(subArticle);
+        return subArticle;
     }
 
 }
